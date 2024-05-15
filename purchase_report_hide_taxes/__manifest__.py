3e40624a38cd843a1e293e8b9c7a34df7a371eb2
Bruno <bruno.botti@kmee.com.br>
2024-05-15 10:12:41 -0300

    [ADD] remove product taxes in purchase report

---
+++ purchase_report_hide_taxes/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Report Hide Taxes",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "report/purchase_report.xml",
+    ],
+}
