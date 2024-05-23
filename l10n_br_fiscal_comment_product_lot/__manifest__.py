3ff7b9a2e018ac700960db76307f58ff2df9f5a9
Bruno <bruno.botti@kmee.com.br>
2024-05-23 16:39:05 -0300

    [NEW] product lot fiscal comment module

---
+++ l10n_br_fiscal_comment_product_lot/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 KMEE
+# License LGPL-3 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Lot Fiscal Comment",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "l10n_br_fiscal",
+        "account_invoice_production_lot",
+    ],
+    "data": [
+        "data/prod_lot_fiscal_comment.xml",
+    ],
+}
---
+++ l10n_br_fiscal_comment_product_lot/models/__init__.py
@@ -0,0 +1,1 @@
+from . import document_fiscal_line_mixin
