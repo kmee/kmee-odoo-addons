e6ab5667478b904f8bfad47dde3bd3c09b750a4d
Luis Felipe Miléo <mileo@kmee.com.br>
2024-05-26 10:51:55 -0300

    [NEW] account_payment_term_manual

---
+++ account_payment_term_manual/models/account_move.py
@@ -0,0 +1,46 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+
+    _inherit = "account.move"
+
+    manual_payment_term_ids = fields.One2many(
+        "account.move.payment.term",
+        "account_move_id",
+        string="Manual Payment Terms",
+    )
+
+    @api.onchange(
+        "line_ids",
+        "invoice_payment_term_id",
+        "invoice_date_due",
+        "invoice_cash_rounding_id",
+        "invoice_vendor_bill_id",
+    )
+    def _onchange_recompute_dynamic_lines(self):
+        super(AccountMove, self)._onchange_recompute_dynamic_lines()
+
+        if self.manual_payment_term_ids:
+            self.manual_payment_term_ids.unlink()
+        manual_payment_term_lines = self.invoice_payment_term_id.compute(
+            self.amount_total, self.invoice_date_due, self.currency_id
+        )
+        for line in manual_payment_term_lines:
+            if line[1]:
+                self.manual_payment_term_ids.create(
+                    {
+                        "account_move_id": self.id,
+                        "date_maturity": line[0],
+                        "amount": line[1],
+                    }
+                )
+
+    def _recompute_payment_terms_lines(self):
+        return super(
+            AccountMove,
+            self.with_context(manual_payment_term_ids=self.manual_payment_term_ids),
+        )._recompute_payment_terms_lines()
