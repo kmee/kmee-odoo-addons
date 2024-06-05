c951db4af0c556c2dc01d4dc5a05301eb70f1798
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-06-05 14:31:34 -0300

    [WIP] change term manual to term line manual

--- account_payment_term_manual/models/account_payment_term.py
+++ account_payment_term_manual/models/account_payment_term.py
@@ -7,12 +7,12 @@ from odoo import models
 class AccountPaymentTerm(models.Model):
     _inherit = "account.payment.term"
 
-    def compute(self, value, date_ref=False, currency=None):
-        manual_payment_term_ids = self.env.context.get("manual_payment_term_ids")
-        if manual_payment_term_ids:
-            sign = -1 if value < 0 else 1
-            return [
-                (line.date_maturity, sign * line.amount)
-                for line in manual_payment_term_ids
-            ]
-        return super().compute(value, date_ref=date_ref, currency=currency)
+    # def compute(self, value, date_ref=False, currency=None):
+    #     manual_term_lines = self.env.context.get("manual_payment_term_line_ids")
+    #     if manual_term_lines:
+    #         sign = -1 if value < 0 else 1
+    #         return [
+    #             (line.date_maturity, sign * line.amount)
+    #             for line in manual_term_lines
+    #         ]
+    #     return super().compute(value, date_ref=date_ref, currency=currency)
