f84db312e89c4903f69373ae449f6dfe732a4695
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-06-05 14:31:34 -0300

    [WIP] change term manual to term line manual

--- account_payment_term_manual/models/account_move.py
+++ account_payment_term_manual/models/account_move.py
@@ -8,10 +8,10 @@ class AccountMove(models.Model):
 
     _inherit = "account.move"
 
-    manual_payment_term_ids = fields.One2many(
-        "account.move.payment.term",
+    manual_payment_term_line_ids = fields.One2many(
+        "account.move.payment.term.line",
         "account_move_id",
-        string="Manual Payment Terms",
+        string="Manual Payment Term Lines",
         copy=False,
     )
 
@@ -23,29 +23,33 @@ class AccountMove(models.Model):
         "invoice_vendor_bill_id",
     )
     def _onchange_recompute_dynamic_lines(self):
-        if self.manual_payment_term_ids:
-            self.manual_payment_term_ids = False
+        term_lines = [tl.copy_data()[0] for tl in self.invoice_payment_term_id.line_ids]
+        new_line_ids = self.manual_payment_term_line_ids.create(term_lines)
+        self.manual_payment_term_line_ids = new_line_ids
 
         super(AccountMove, self)._onchange_recompute_dynamic_lines()
 
-        if self.invoice_payment_term_id:
-            manual_payment_term_lines = self.invoice_payment_term_id.compute(
-                self.amount_total, self.invoice_date, self.currency_id
-            )
-            for line in manual_payment_term_lines:
-                if line[1] > 0:
-                    self.manual_payment_term_ids += self.env[
-                        "account.move.payment.term"
-                    ].create(
-                        {
-                            "account_move_id": self.id,
-                            "date_maturity": line[0],
-                            "amount": abs(line[1]),
-                        }
-                    )
+        # if self.manual_payment_term_line_ids:
+        #     self.manual_payment_term_line_ids = False
 
-    def _recompute_payment_terms_lines(self):
-        return super(
-            AccountMove,
-            self.with_context(manual_payment_term_ids=self.manual_payment_term_ids),
-        )._recompute_payment_terms_lines()
+        # if self.invoice_payment_term_id:
+        #     manual_payment_term_lines = self.invoice_payment_term_id.compute(
+        #         self.amount_total, self.invoice_date, self.currency_id
+        #     )
+        #     for line in manual_payment_term_lines:
+        #         if line[1] > 0:
+        #             self.manual_payment_term_line_ids += self.env[
+        #                 "account.move.payment.term"
+        #             ].create(
+        #                 {
+        #                     "account_move_id": self.id,
+        #                     "date_maturity": line[0],
+        #                     "amount": abs(line[1]),
+        #                 }
+        #             )
+
+    # def _recompute_payment_terms_lines(self):
+    #     return super(
+    #         AccountMove,
+    #         self.with_context(manual_payment_term_line_ids=self.manual_payment_term_line_ids),
+    #     )._recompute_payment_terms_lines()
--- account_payment_term_manual/models/account_payment_term_manual_mixin.py
+++ account_payment_term_manual/models/account_payment_term_manual_mixin.py
@@ -1,16 +1,16 @@
 # Copyright 2024 KMEE
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import models
 
 
 class AccountPaymentTermManualMixin(models.AbstractModel):
 
-    _name = "account.payment.term.manual.mixin"
+    _name = "account.payment.term.line.manual.mixin"
 
-    currency_id = fields.Many2one(
-        "res.currency",
-        readonly=True,
-    )
-    date_maturity = fields.Date()
-    amount = fields.Monetary()
+    # currency_id = fields.Many2one(
+    #     "res.currency",
+    #     readonly=True,
+    # )
+    # date_maturity = fields.Date()
+    # amount = fields.Monetary()
