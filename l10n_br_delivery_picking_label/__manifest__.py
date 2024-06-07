6ccaf16138fb89786c0a81f278b04bf8a31592fb
Tiago Amaral <tiago.amaral@kmee.com.br>
2024-06-07 10:08:13 -0300

    [ADD] Addons: l10n_br_delivery_picking_label

---
+++ l10n_br_delivery_picking_label/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Delivery Picking Label",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "l10n_br_stock_account",
+        "l10n_br_delivery_nfe",
+    ],
+    "data": [
+        "report/report_delivery_picking_label.xml",
+        "data/paperformat.xml",
+        "data/report_delivery_picking_label.xml",
+    ],
+    "demo": [],
+}
