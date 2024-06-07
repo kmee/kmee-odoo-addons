7c56309633160d71b71bccf55eeb83130d523c7d
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-06-07 18:04:28 -0300

    [ADD] create invoice specific pay_term and logic

--- account_payment_term_manual/models/account_payment_term.py
+++ account_payment_term_manual/models/account_payment_term.py
@@ -7,12 +7,11 @@ from odoo import models
 class AccountPaymentTerm(models.Model):
     _inherit = "account.payment.term"
 
-    # def compute(self, value, date_ref=False, currency=None):
-    #     manual_term_lines = self.env.context.get("manual_payment_term_line_ids")
-    #     if manual_term_lines:
-    #         sign = -1 if value < 0 else 1
-    #         return [
-    #             (line.date_maturity, sign * line.amount)
-    #             for line in manual_term_lines
-    #         ]
-    #     return super().compute(value, date_ref=date_ref, currency=currency)
+    def compute(self, value, date_ref=False, currency=None):
+        """Inherit compute to use manual term id if set."""
+        manual_payment_term_id = self.env.context.get("manual_payment_term_id")
+        if manual_payment_term_id and self is not manual_payment_term_id:
+            return manual_payment_term_id.compute(
+                value, date_ref=date_ref, currency=currency
+            )
+        return super().compute(value, date_ref=date_ref, currency=currency)
