6ded1b9d5e3b2c9a3e7982a587ba89a50f57a8b8
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-06-07 18:04:28 -0300

    [ADD] create invoice specific pay_term and logic

--- account_payment_term_manual/models/__init__.py
+++ account_payment_term_manual/models/__init__.py
@@ -1,4 +1,4 @@
 # from . import account_payment_term_manual_mixin
 from . import account_move
 from . import account_payment_term
-from . import account_move_payment_term_line
+from . import account_move_payment_term
