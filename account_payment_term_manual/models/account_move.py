32e81badb1c23a8a4fdbc0009810352f87ba0d33
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-06-10 17:49:47 -0300

    [ADD] payment term manual mixin

--- account_payment_term_manual/models/account_move.py
+++ account_payment_term_manual/models/account_move.py
@@ -1,62 +1,16 @@
 # Copyright 2024 KMEE
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class AccountMove(models.Model):
 
-    _inherit = "account.move"
-
-    manual_payment_term_id = fields.Many2one(
-        comodel_name="account.payment.term.manual",
-        string="Manual Payment Term",
-        copy=False,
-    )
-
-    manual_payment_term_line_ids = fields.One2many(
-        comodel_name="account.payment.term.line.manual",
-        related="manual_payment_term_id.line_ids",
-        string="Manual Payment Term Lines",
-    )
-
-    def _update_manual_payment_term_id(self):
-        """Check if invoice term has changed and replace manual term if needed."""
-        if (
-            self.manual_payment_term_id.mapped("origin_term_id")
-            == self.invoice_payment_term_id
-        ):
-            # Do nothing if manual term is already based on current term_id
-            return
-
-        self = self.with_context(skip_manual_term_onchange=True)
-        # Unlink and create seem to reset invoice_payment_term_id, here we store it
-        # in a variable to set the term_id again at the END OF THE METHOD
-        new_inv_term_id = self.invoice_payment_term_id
-
-        # THIS SECTION IS ORDER SENSITIVE!
-        new_manual_term = self.invoice_payment_term_id.copy_data()[0]
-        new_manual_term["origin_term_id"] = self.invoice_payment_term_id.id
-        new_manual_term["active"] = False
-        new_manual_term["note"] = ""
-        # Split creation for term and term.lines
-        new_line_ids = new_manual_term.pop("line_ids")
-        new_manual_term["line_ids"] = False
-
-        # DO NOT MOVE THIS UNLINK
-        self.manual_payment_term_id.line_ids.unlink()
-        self.manual_payment_term_id.unlink()
-        # DO NOT MOVE THIS CREATE
-        manual_term_id = self.manual_payment_term_id.create(new_manual_term)
-        for line in new_line_ids:
-            line[2]["manual_payment_id"] = manual_term_id.id
-        manual_term_id.line_ids = manual_term_id.line_ids.create(
-            nl[2] for nl in new_line_ids
-        )
-        self.manual_payment_term_id = manual_term_id
-
-        # END OF THE METHOD -> ensure term_id gets set to user defined value
-        self.invoice_payment_term_id = new_inv_term_id
+    _name = "account.move"
+    _inherit = [
+        "account.move",
+        "account.payment.term.manual.mixin",
+    ]
 
     @api.onchange(
         "line_ids",
@@ -67,8 +21,16 @@ class AccountMove(models.Model):
     )
     def _onchange_recompute_dynamic_lines(self):
         # Replace manual lines if invoice term has changed
-        if not self.env.context.get("skip_manual_term_generation"):
-            self._update_manual_payment_term_id()
+        if self.env.context.get("payment_term_id_view_onchange"):
+            # Unlink and create seem to reset invoice_payment_term_id, here we store it
+            # in a variable to set the term_id again at the END OF THE METHOD
+            new_inv_term_id = self.invoice_payment_term_id
+
+            # unlinks/creates happen in this method: _update_manual_payment_term_id
+            self._update_manual_payment_term_id(self.invoice_payment_term_id)
+
+            # END OF THE METHOD -> ensure term_id gets set to user defined value
+            self.invoice_payment_term_id = new_inv_term_id
 
         super(AccountMove, self)._onchange_recompute_dynamic_lines()
 
@@ -80,18 +42,11 @@ class AccountMove(models.Model):
 
     @api.onchange("manual_payment_term_id")
     def _onchange_manual_payment_term_id(self):
-        # Do nothing if new manual term was just duplicated from term_id
-        if self.env.context.get("skip_manual_term_onchange"):
-            return
-
-        # Append (edited) to manual term name if it was manually edited
-        if (
-            self.manual_payment_term_id
-            and not self.manual_payment_term_id.has_manual_lines
-        ):
-            self.manual_payment_term_id.has_manual_lines = True
-            self.manual_payment_term_id.name += " (edited)"
+        """Inherit mixins onchange to add invoice line recomputes"""
+        res = super()._onchange_manual_payment_term_id()
 
         # Recompute with context
-        self = self.with_context(skip_manual_term_generation=True)
+        # self = self.with_context(skip_manual_term_generation=True)
         self._onchange_recompute_dynamic_lines()
+
+        return res
