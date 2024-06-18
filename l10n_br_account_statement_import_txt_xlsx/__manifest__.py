e3ce81fdf7f35a616cdeb2ced05d3ea04d89ce33
Luis Felipe Miléo <mileo@kmee.com.br>
2024-06-18 12:11:37 -0300

    [NEW][14.0][WIP] bank statment import

---
+++ l10n_br_account_statement_import_txt_xlsx/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Account Statement Import Txt Xlsx",
+    "summary": """
+        Importação de Extratos Bancários TXT/XLS dos Bancos Brasileiros""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "account_statement_import_txt_xlsx",
+    ],
+    "data": [
+        "data/map_data.xml",
+    ],
+}
