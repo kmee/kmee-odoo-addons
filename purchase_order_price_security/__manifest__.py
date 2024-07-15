f4d4c7c156d700f40b08cf7d46bc54c63b2c6d9b
Tiago Amaral <tiago.amaral@kmee.com.br>
2024-07-15 09:43:07 -0300

    [ADD] Addon: purchase_order_price_security

---
+++ purchase_order_price_security/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Order Price Security",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "security/groups.xml",
+        "views/purchase_order_line.xml",
+    ],
+    "demo": [],
+}
