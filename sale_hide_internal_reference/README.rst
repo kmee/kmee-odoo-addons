047adb2476f7713340e7d6598e32a8d0991546b9
Tiago Amaral <tiago.amaral@kmee.com.br>
2024-07-26 15:53:59 -0300

    [ADD] sale_hide_internal_reference

---
+++ sale_hide_internal_reference/README.rst
@@ -0,0 +1,23 @@
+============================
+Sale Hide Internal Reference
+============================
+
+Sale Hide Internal Reference
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+#. Go to ...
+
+Usage
+=====
+
+To use this module, you need to:
+
+#. Go to ...
+
+
+Changelog
+=========
---
+++ sale_hide_internal_reference/models/product_product.py
@@ -0,0 +1,26 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    hide_internal_reference = fields.Boolean("Hide Internal Reference")
+
+
+class ProductProduct(models.Model):
+
+    _inherit = "product.product"
+
+    def get_product_multiline_description_sale(self):
+        if self.product_tmpl_id.hide_internal_reference:
+            name = self.name
+        else:
+            name = self.display_name
+        if self.description_sale:
+            name += "\n" + self.description_sale
+
+        return name
