9588f83cc3d584244f26d17d2a9a9de5f30ecd9b
Luis Felipe Miléo <mileo@kmee.com.br>
2024-08-03 16:51:51 -0300

    [NEW] consultancy_sale_project
    Signed-off-by: Luis Felipe Miléo <mileo@kmee.com.br>

---
+++ consultancy_sale_project/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Consultancy Crm",
+    "description": """
+        Consultancy CRM""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-private-addons",
+    "depends": [
+        "sale_project",
+        "sale_timesheet",
+        "contract_sale",
+        "contract_variable_quantity",
+        "contract_payment_mode",
+        "product_contract",
+        "subscription_oca",
+    ],
+    "data": [],
+    "demo": [],
+}
