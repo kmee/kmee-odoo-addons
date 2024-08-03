fc8042c25669a35c6491ed61f4e39a7509ccd422
Luis Felipe Miléo <mileo@kmee.com.br>
2024-08-03 16:51:33 -0300

    [NEW] consultancy_crm
    Signed-off-by: Luis Felipe Miléo <mileo@kmee.com.br>

---
+++ consultancy_crm/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Consultancy Crm",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-private-addons",
+    "depends": [
+        "crm_timesheet",
+        "crm_lead_code",
+        "crm_lead_to_task",
+        "crm_project_task",
+        "crm_phonecall",
+        "crm_lead_search_archive",
+        "marketing_crm_partner",
+        # "crm_exception",
+        # "crm_stage_probability",
+    ],
+    "data": [],
+    "demo": [],
+}
