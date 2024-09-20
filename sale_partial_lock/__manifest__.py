32e62ebe53bf319e360a5b49e25ecd183c874de7
Tiago Amaral <tiago.amaral@kmee.com.br>
2024-09-20 19:10:17 -0300

    [ADD] Addon: sale_partial_lock

---
+++ sale_partial_lock/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Partial Lock",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE, Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "demo": [],
+}
