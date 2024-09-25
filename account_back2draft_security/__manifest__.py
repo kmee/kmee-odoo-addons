a2cd9ac33c5434c1a170fbddc6025fe3802b64bf
Bruno <bruno.botti@kmee.com.br>
2024-09-25 10:58:17 -0300

    [ADD] account_back2draft_security: new module

---
+++ account_back2draft_security/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Back2draft Security",
+    "summary": """
+        Hide back to draft invoice button based on group""",
+    "version": "15.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "security/invoice_button_security.xml",
+    ],
+    "demo": [],
+}
