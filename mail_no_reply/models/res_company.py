1b0e99c383d36620734c4fa07e414c4e18bc7ff2
Bruno <bruno.botti@kmee.com.br>
2024-09-26 23:53:10 -0300

    [ADD] mail_no_reply: new module

---
+++ mail_no_reply/models/res_company.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    email_no_reply = fields.Char(
+        string="No-Reply Email",
+        default="no-reply@empresa.com.br",
+    )
