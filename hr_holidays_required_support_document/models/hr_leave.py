c9c38d3d777308450813df1458761f3574638446
Bruno <bruno.botti@kmee.com.br>
2024-10-15 15:55:53 -0300

    [FIX] hr_holidays_required_support_document: unable to create the time off register

--- hr_holidays_required_support_document/models/hr_leave.py
+++ hr_holidays_required_support_document/models/hr_leave.py
@@ -1,11 +1,36 @@
-from odoo import _, models
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class HolidaysRequest(models.Model):
     _inherit = "hr.leave"
 
-    def action_approve(self):
+    supported_attachment_ids = fields.Many2many(
+        "ir.attachment",
+        string="Supporting Documents",
+        help="Attach any supporting documents here.",
+    )
+
+    @api.model
+    def create(self, vals):
+        # Cria o registro do leave
+        leave = super(HolidaysRequest, self).create(vals)
+
+        # Associa os anexos ao registro criado
+        if vals.get("supported_attachment_ids"):
+            leave.supported_attachment_ids.write(
+                {
+                    "res_id": leave.id,
+                    "res_model": "hr.leave",
+                }
+            )
+
+        return leave
+
+    def write(self, vals):
+        # Atualiza o registro do leave
+        res = super(HolidaysRequest, self).write(vals)
+
         for record in self:
             if (
                 record.holiday_status_id.required_support_document
@@ -14,4 +39,14 @@ class HolidaysRequest(models.Model):
                 raise ValidationError(
                     _("Unable to approve, supporting document was not attached")
                 )
-        return super(HolidaysRequest, self).action_approve()
+
+            # Verifica e associa os anexos ao registro se necessário
+            if "supported_attachment_ids" in vals:
+                record.supported_attachment_ids.write(
+                    {
+                        "res_id": record.id,
+                        "res_model": "hr.leave",
+                    }
+                )
+
+        return res
