c727f36daca5269adfcece079e5c277dc620a119
Tiago Amaral <tiagoamral23@gmail.com>
2024-10-29 09:28:13 -0300

    [IMP] sale_order_pos_report: add button to show pdf in another tab.

---
+++ sale_order_pos_report/controllers/sale_order_pos_report.py
@@ -0,0 +1,20 @@
+from odoo import http
+from odoo.http import request
+
+
+class SaleOrderPosController(http.Controller):
+    @http.route("/sale_order_pos_report/<int:sale_order_id>", type="http", auth="user")
+    def store_ticket_report(self, sale_order_id, **post):
+        sale_order = request.env["sale.order"].browse(sale_order_id)
+        if not sale_order.exists():
+            return request.not_found()
+
+        pdf = request.env.ref(
+            "sale_order_pos_report.action_report_saleorder_compact"
+        )._render_qweb_pdf(sale_order.ids)[0]
+        response = request.make_response(pdf)
+        response.headers["Content-Type"] = "application/pdf"
+        response.headers[
+            "Content-Disposition"
+        ] = f'inline; filename="sale_order_{sale_order_id}.pdf"'
+        return response
