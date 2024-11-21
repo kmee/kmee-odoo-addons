317313357860eecdb5f6560258abb652a43b5e6f
Luis Felipe Miléo <mileo@kmee.com.br>
2024-11-21 08:12:37 -0300

    [NEW] hr_holidays_plan_next_year
    Signed-off-by: Luis Felipe Miléo <mileo@kmee.com.br>

---
+++ hr_holidays_plan_next_year/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Hr Holidays Plan Next Year",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "hr_holidays",
+    ],
+}
