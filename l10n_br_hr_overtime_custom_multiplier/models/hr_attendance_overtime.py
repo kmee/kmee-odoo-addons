2179dcb07e5c671c741fe2527f72919aacca6a66
Luis Felipe Miléo <mileo@kmee.com.br>
2024-11-27 14:04:57 -0300

    [REF] custom_multiplier
    Signed-off-by: Luis Felipe Miléo <mileo@kmee.com.br>

--- l10n_br_hr_overtime_custom_multiplier/models/hr_attendance_overtime.py
+++ l10n_br_hr_overtime_custom_multiplier/models/hr_attendance_overtime.py
@@ -1,13 +1,15 @@
 # Copyright 2024 KMEE
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class HrAttendanceOvertime(models.Model):
 
     _inherit = "hr.attendance.overtime"
 
-    extra_hours_withou_multiplier = fields.Float(string='Extra Hours without Multiplier')
-    
-    applied_multiplier = fields.Float(string='Applied Multiplier', default=1.0)
+    extra_hours_withou_multiplier = fields.Float(
+        string="Extra Hours without Multiplier"
+    )
+
+    applied_multiplier = fields.Float(string="Applied Multiplier", default=1.0)
