ec4f2a9ada2c18193ec80c410ab8281c7c076d3d
Tiago Amaral <tiagoamral23@gmail.com>
2024-12-16 10:58:26 -0300

    [ADD] Addon: account_statement_import_ofx_invert

---
+++ account_statement_import_ofx_invert/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Statement Import Ofx Invert",
+    "summary": """KMEE""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "account_statement_import_ofx",
+    ],
+    "data": [
+        "views/account_journal.xml",
+    ],
+    "demo": [],
+}
