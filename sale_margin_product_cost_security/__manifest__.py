d28424ca96581c23e7a3c3bca7ba1a9369f3fa90
Tiago Amaral <tiagoamral23@gmail.com>
2024-12-20 08:38:53 -0300

    [ADD] Addon: sale_margin_product_cost_security

---
+++ sale_margin_product_cost_security/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Product Cost Security",
+    "summary": """KMEE""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["sale_margin", "product_cost_security"],
+    "data": [
+        "views/sale_order_line_views.xml",
+    ],
+    "demo": [],
+}
