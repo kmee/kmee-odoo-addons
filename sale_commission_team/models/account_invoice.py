fcf50f26d50729d9b1170a017e14b427fcd5a223
Luis Felipe Miléo <mileo@kmee.com.br>
2025-01-26 17:50:14 -0300

    [NEW] sale_commission_team

---
+++ sale_commission_team/models/account_invoice.py
@@ -0,0 +1,20 @@
+# Copyright 2021 KMEE - Luis Felipe Mileo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.move.line'
+
+    def _prepare_agents_vals_partner(self, partner_id):
+        """Add salesman agent if configured so and no other commission
+        already populated.
+        """
+        res = super()._prepare_agents_vals_partner(partner_id)
+        if not res:
+            if partner_id and self.team_id:
+                return self._prepare_agents_team_vals_partner(
+                    partner_id, self.team_id
+                )
+        return res
