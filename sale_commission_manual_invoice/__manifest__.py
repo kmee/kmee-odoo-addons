4703fb2545e9f46ebf76c91dacd9be0ce1edc925
Luis Felipe Miléo <mileo@kmee.com.br>
2025-01-26 17:36:14 -0300

    [NEW] sale_commission_manual_invoice

---
+++ sale_commission_manual_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Commission Manual Invoice",
+    "description": """
+        Sale Commission Manual Invoice""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["sale_commission"],
+    "data": [
+        "views/sale_commission_settlement.xml",
+    ],
+    "demo": [],
+}
