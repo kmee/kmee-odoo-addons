e903d11eb84abd8e16d1e99d13f337e0ade3f473
Luis Felipe Miléo <mileo@kmee.com.br>
2025-02-02 11:47:50 -0300

    [IMP] sale_commission_team

--- sale_commission_team/models/sale_commission_mixin.py
+++ sale_commission_team/models/sale_commission_mixin.py
@@ -10,21 +10,45 @@ class SaleCommissionMixin(models.AbstractModel):
     @api.model
     def _prepare_agents_team_vals_partner(self, partner_id, team_id):
         """Utility method for getting agents of a partner."""
-
-        agent_team_ids = partner_id.agent_team_ids.filtered(
-            lambda x: x.team_id == team_id
+        agent_team_ids = partner_id.agent_team_ids.filtered_domain(
+            [("team_id", "=", team_id.id)]
         )
-        # TODO: Limit one partner/agent/team by configuration
-        rec = []
-        for agent_team_id in agent_team_ids:
-            rec.append(
-                (
-                    0,
-                    0,
-                    {
-                        "agent": agent_team_id.agent_id.id,
-                        "commission": agent_team_id.commission.id,
-                    },
-                )
+        if not agent_team_ids:
+            agent_team_ids = self.env["crm.team.agent"].search(
+                [("team_id", "=", team_id.id)]
             )
-        return rec
+
+        return [
+            (
+                0,
+                0,
+                {
+                    "agent_id": agent.agent_id.id,
+                    "commission_id": agent.commission_id.id,
+                },
+            )
+            for agent in agent_team_ids
+        ]
+
+    def _compute_agents_with_team(self, team, partner_id, base_agents):
+        """Generalized method for merging agents from sales/invoice teams."""
+        if not (partner_id and team):
+            return base_agents
+
+        team_agents = self._prepare_agents_team_vals_partner(partner_id, team)
+
+        if team.only_team_agents:
+            return team_agents
+
+        existing_agents = {agent[2]["agent_id"]: agent for agent in base_agents}
+
+        for team_agent in team_agents:
+            agent_id = team_agent[2]["agent_id"]
+            if agent_id in existing_agents:
+                existing_agents[agent_id][2]["commission_id"] = team_agent[2][
+                    "commission_id"
+                ]
+            else:
+                base_agents.append(team_agent)
+
+        return base_agents
