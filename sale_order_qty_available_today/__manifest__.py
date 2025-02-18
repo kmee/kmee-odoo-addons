673091f61630e6e71c636d8477f2b4825db921ed
Tiago Amaral <tiagoamral23@gmail.com>
2025-02-18 09:16:49 -0300

    [ADD] Addon: sale_order_qty_available_today

---
+++ sale_order_qty_available_today/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Qty Available Today",
+    "summary": """Sale Order Free Qty""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["sale_stock"],
+    "data": [
+        "views/sale_order_line.xml",
+    ],
+    "demo": [],
+}
