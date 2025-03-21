aa46127b1b0fc552c869f5751216acd30a9bc17b
Luis Felipe Miléo <mileo@kmee.com.br>
2025-03-20 23:00:01 -0300

    [NEW] crm_bant

---
+++ crm_bant/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "CRM BANT Qualification",
+    "version": "14.0.1.0.0",
+    "category": "Sales/CRM",
+    "author": "KMEE, Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "crm",
+        "spreadsheet_dashboard",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+        "views/dashboard_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
---
+++ crm_bant/readme/USAGE.md
@@ -0,0 +1,6 @@
+To use this module, you need to:
+
+1. Go to **Account**.
+2. Click on the **Import(OCA)** button for the bank account.
+3. Click on the **UPLOAD YOUR FILE** button and select the file.
+4. Click on the **IMPORT AND VIEW** button.
