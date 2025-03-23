d07d7be05c86959c124358b94c09934aca8b2707
Tiago Amaral <tiagoamral23@gmail.com>
2025-03-23 15:02:58 -0300

    [NEW] Addon: sale_tier_validation_exceptions

---
+++ sale_tier_validation_exceptions/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Tier Validation Exceptions",
+    "summary": """Sale Tier Validation Exceptions""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE, Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "sale_exception",
+        "sale_tier_validation",
+        "report_py3o",
+    ],
+    "data": [
+        "data/tier_definition.xml",
+        "views/sale_exception_confirm.xml",
+        "views/sale_order.xml",
+        "views/exception_rule.xml",
+    ],
+    "demo": [],
+}
