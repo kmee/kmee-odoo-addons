ce661a08a0efd0052e7ae65acc7e8e15cf62285a
DiegoParadeda <DiegoParadeda@users.noreply.github.com>
2025-03-27 15:12:14 -0300

    [ADD] account_custom_payment_term: custom payment (#138)
    terms functionality

---
+++ account_custom_payment_term/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Account Custom Payment Term",
+    "summary": "Adds record-specific editable payment terms that are archived by default.",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "author": "KMEE",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "data": [
+        "views/account_payment_term_views.xml",
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
