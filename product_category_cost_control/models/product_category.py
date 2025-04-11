64c8cc2b28e80e9c44faefe2c6bda3c98a84a29a
Diego Paradeda <diego.paradeda@kmee.com.br>
2025-04-11 15:39:03 -0300

    [ADD] product_category_cost_control: custom AVCO

---
+++ product_category_cost_control/models/product_category.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2025-Today - KMEE (<http://www.kmee.com.br>).
+# @author Diego Paradeda <diego.paradeda@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+
+    price_control_option = fields.Selection(
+        [
+            ("include_all_taxes", "Include All Taxes"),
+            ("remove_included_taxes", "Remove Included Taxes"),
+        ],
+        string="Price Control Option",
+        help="Defines how the price unit is calculated for products in this category.",
+    )
---
+++ product_category_cost_control/readme/USAGE.md
@@ -0,0 +1,5 @@
+To use this module you need:
+
+    1. Configure the product category with AVCO automated;
+    2. Generate a purchase and the stock receipt;
+    3. Review updated product cost.
