654cd30f56462367789264f565218dfb26ae39e6
Diego Paradeda <diego.paradeda@kmee.com.br>
2025-04-28 16:03:31 -0300

    [ADD] new addon sale_total_quantity

---
+++ sale_total_quantity/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Sale Total Quantity",
+    "summary": "Displays the total quantity of products in the sale order.",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "author": "KMEE",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
