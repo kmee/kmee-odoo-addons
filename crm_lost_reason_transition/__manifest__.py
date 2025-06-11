4ace7f8932d45b0f08757aaa82560bcc8e118b66
Ygor Carvalho <ygcarvalh@gmail.com>
2025-06-11 17:49:56 -0300

    docs: update summary module description

--- crm_lost_reason_transition/__manifest__.py
+++ crm_lost_reason_transition/__manifest__.py
@@ -6,6 +6,10 @@
     "license": "AGPL-3",
     "author": "KMEE, Odoo Community Association (OCA)",
     "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "summary": """
+        Automatically route lost leads to specific teams and stages based on
+        the loss reason and current stage.
+    """,
     "depends": [
         "crm",
     ],
