9908eca605b02aa65a8fb982e1f2fec90137e5c9
Ygor Carvalho <ygcarvalh@gmail.com>
2025-06-17 19:37:53 -0300

    feat: update the model to see icon in forms

--- partner_customer_rating/models/res_partner.py
+++ partner_customer_rating/models/res_partner.py
@@ -1,15 +1,24 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
+
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     customer_rating_id = fields.Many2one(
-        'customer.rating',
-        string='Satisfação do Cliente',
-        tracking=True
+        "customer.rating", string="Satisfação do Cliente", tracking=True
     )
+
+    image_rating = fields.Binary(
+        string="Satisfação",
+        compute="_compute_image_rating",
+    )
+
+    @api.depends("customer_rating_id.image")
+    def _compute_image_rating(self):
+        for rec in self:
+            rec.image_rating = rec.customer_rating_id.image
 
     @api.model
     def _search_customer_rating(self, operator, value):
-        rating_ids = self.env['customer.rating'].search([('name', operator, value)]).ids
-        return [('customer_rating_id', 'in', rating_ids)]
+        rating_ids = self.env["customer.rating"].search([("name", operator, value)]).ids
+        return [("customer_rating_id", "in", rating_ids)]
