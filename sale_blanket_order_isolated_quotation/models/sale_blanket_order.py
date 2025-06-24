e0577001ebe3f7a8af701e6223064b9adaa2423f
Tiago Amaral <tiagoamral23@gmail.com>
2025-06-24 08:30:07 -0300

    [REF] sale_blanket_order_isolated_quotation

--- sale_blanket_order_isolated_quotation/models/sale_blanket_order.py
+++ sale_blanket_order_isolated_quotation/models/sale_blanket_order.py
@@ -42,26 +42,34 @@ class SaleBlanketOrder(models.Model):
     def _prepare_lines_to_sale_order_line(self, line, matched_line):
         line.qty = matched_line.product_uom_qty
 
-    def action_create_increment_quotation(self):
-        """Create a new quotation for incrementing the blanket order."""
+    def _prepare_common_blanket_order_fields(self, override_vals=None):
         self.ensure_one()
+        blanket_order_fields = set(self.env["sale.blanket.order"].fields_get().keys())
+        sale_order_fields = set(self.env["sale.order"].fields_get().keys())
+        common_fields = blanket_order_fields.intersection(sale_order_fields)
+        vals = {"blanket_order_id": self.id}
+        skip_fields = self._get_skip_fields()
+        for field in common_fields:
+            if field not in skip_fields:
+                value = getattr(self, field)
+                vals[field] = value.id if hasattr(value, "id") else value
+        if override_vals:
+            vals.update(override_vals)
+        return vals
 
-        # Create sale order with lines from blanket order
-        vals = {
-            "partner_id": self.partner_id.id,
-            "is_blanket_order_increment": True,
-            "blanket_order_id": self.id,
-            "blanket_order_type": "amendment",  # Forçar tipo amendment
-        }
-
+    def action_create_increment_quotation(self):
+        self.ensure_one()
+        vals = self._prepare_common_blanket_order_fields(
+            {
+                "is_blanket_order_increment": True,
+                "blanket_order_type": "amendment",
+            }
+        )
         sale_order = self.env["sale.order"].create(vals)
-
-        # Create sale order lines
         for line in self.line_ids:
             vals = sale_order._prepare_blanket_order_line_values(line)
             vals["order_id"] = sale_order.id
             self.env["sale.order.line"].create(vals)
-
         return {
             "type": "ir.actions.act_window",
             "name": "Increment Quotation",
@@ -73,26 +81,16 @@ class SaleBlanketOrder(models.Model):
 
     def action_create_reference_quotation(self):
         self.ensure_one()
-
-        blanket_order_fields = set(self.env["sale.blanket.order"].fields_get().keys())
-        sale_order_fields = set(self.env["sale.order"].fields_get().keys())
-        common_fields = blanket_order_fields.intersection(sale_order_fields)
-        vals = {
-            "blanket_order_id": self.id,
-            "blanket_order_type": "reference",
-        }
-        skip_fields = self._get_skip_fields()
-        for field in common_fields:
-            if field not in skip_fields:
-                value = getattr(self, field)
-                vals[field] = value.id if hasattr(value, "id") else value
+        vals = self._prepare_common_blanket_order_fields(
+            {
+                "blanket_order_type": "reference",
+            }
+        )
         sale_order = self.env["sale.order"].create(vals)
-
         for line in self.line_ids:
             line_vals = sale_order._prepare_blanket_order_line_values(line)
             line_vals["order_id"] = sale_order.id
             self.env["sale.order.line"].create(line_vals)
-
         return {
             "type": "ir.actions.act_window",
             "name": "Reference Quotation",
