ad27fc51ed60710e5df02b043a3bcbed20104b64
Tiago Amaral <tiagoamral23@gmail.com>
2025-07-08 07:09:43 -0300

    [IMP] sale_blanket_order_isolated_quotation: provisional translation revert this commit in the future

--- sale_blanket_order_isolated_quotation/models/sale_order.py
+++ sale_blanket_order_isolated_quotation/models/sale_order.py
@@ -49,7 +49,7 @@ class SaleOrder(models.Model):
             return self._confirm_blanket_order_increment()
         elif not self.order_sequence:  # É um orçamento
             return {
-                "name": _("Confirm Sale Order"),
+                "name": _("Confirmar"),
                 "type": "ir.actions.act_window",
                 "res_model": "sale.order.confirm",
                 "view_mode": "form",
