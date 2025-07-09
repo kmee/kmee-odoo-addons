adc165c87f7eda6bbf52e465722158b64e6e648d
Tiago Amaral <tiagoamral23@gmail.com>
2025-07-09 20:33:20 -0300

    [NEW] Addon: l10n_br_tax_manual_values Just create the module and add the dependencies

---
+++ l10n_br_tax_manual_values/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Tax Manual Values",
+    "summary": """l10n_br_tax_manual_values""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "l10n_br_account",
+        "l10n_br_fiscal",
+    ],
+    "data": [],
+    "demo": [],
+}
