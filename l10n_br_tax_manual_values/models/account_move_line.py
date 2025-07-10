381edf770d60675d029a83f695232fcc5f99a364
Tiago Amaral <tiagoamral23@gmail.com>
2025-07-09 22:29:59 -0300

    [IMP] l10n_br_tax_manual_values: add methods

--- l10n_br_tax_manual_values/models/account_move_line.py
+++ l10n_br_tax_manual_values/models/account_move_line.py
@@ -3,7 +3,20 @@
 
 from odoo import models
 
+from .l10n_br_fiscal_document_line_mixin_methods import FISCAL_TAX_PREFIXES
+
 
 class AccountMoveLine(models.Model):
 
     _inherit = "account.move.line"
+
+    def _get_manual_tax_values_from_context(self):
+        tax_values = {}
+        suffixes = ["_base_manual", "_value_manual"]
+
+        for tax_prefix in FISCAL_TAX_PREFIXES:
+            for suffix in suffixes:
+                attr_name = tax_prefix + suffix
+                tax_values[attr_name] = self.env.context.get(attr_name)
+
+        return tax_values
--- l10n_br_tax_manual_values/models/l10n_br_fiscal_document_line_mixin_methods.py
+++ l10n_br_tax_manual_values/models/l10n_br_fiscal_document_line_mixin_methods.py
@@ -3,7 +3,40 @@
 
 from odoo import models
 
+FISCAL_TAX_PREFIXES = [
+    "icms",
+    "icmsst",
+    "issqn",
+    "issqn_wh",
+    "icmsst_wh",
+    "ipi",
+    "ii",
+    "cofins",
+    "cofinsst",
+    "cofins_wh",
+    "pis",
+    "pisst",
+    "pis_wh",
+    "csll",
+    "csll_wh",
+    "irpj",
+    "irpj_wh",
+    "inss",
+    "inss_wh",
+]
+
 
 class FiscalDocumentLineMixinMethods(models.AbstractModel):
 
     _inherit = "l10n_br_fiscal.document.line.mixin.methods"
+
+    def _prepare_br_manual_tax_dict(self):
+        manual_tax_dict = {}
+        suffixes = ["_base_manual", "_value_manual"]
+
+        for tax_prefix in FISCAL_TAX_PREFIXES:
+            for suffix in suffixes:
+                attr_name = tax_prefix + suffix
+                manual_tax_dict[attr_name] = getattr(self, attr_name)
+
+        return manual_tax_dict
