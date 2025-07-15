3721773fc3c58b7813b2825be16fa7ec8d781e27
Luis Felipe Miléo <mileo@kmeee.com.br>
2025-07-15 18:22:20 -0300

    [NEW] cycle

---
+++ helpdesk_mgmt_ticket_life_cycle/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Help Desk Ticket  Stage Duration",
+    "version": "16.0.1.0.0",
+    "summary": "Shows the duration each ticket spent in each stage",
+    "category": "helpdesk",
+    "author": "Ananias, KMEE",
+    "website": "https://github.com/KMEE/kmee-private-addons",
+    "license": "AGPL-3",
+    "depends": ["helpdesk_mgmt"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/helpdesk_ticket_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
--- hr_benefit/README.rst
+++ hr_benefit/README.rst
@@ -55,9 +55,9 @@ Authors
 Contributors
 ------------
 
--  ``KMEE <https://www.kmee.com.br>``\ \_:
+- ``KMEE <https://www.kmee.com.br>``\ \_:
 
-   -  Bruno Corredato Botti
+  - Bruno Corredato Botti
 
 Maintainers
 -----------
