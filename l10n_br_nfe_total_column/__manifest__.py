419c0e08e4bc439138b0dd522456f80f6a11d049
Bruno <bruno.botti@kmee.com.br>
2025-07-15 12:07:04 -0300

    [ADD] l10n_br_nfe_total_column: new module

---
+++ l10n_br_nfe_total_column/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Fiscal Total Column",
+    "summary": """fiscal total column""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "l10n_br_nfe",
+    ],
+    "data": [
+        "views/l10n_br_fiscal.document_tree.xml",
+    ],
+}
