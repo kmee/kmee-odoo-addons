c47258c71729da7b1fde4b2d700e5f2a50d47e66
Tiago Amaral <tiagoamral23@gmail.com>
2025-07-16 16:04:33 -0300

    [IMP] mail_role_filter: pre-commit

--- mail_role_filter/__manifest__.py
+++ mail_role_filter/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE",
-    "website": "https://kmee.com.br/",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
     "depends": ["mail", "base"],
     "data": [
         "security/ir.model.access.csv",
