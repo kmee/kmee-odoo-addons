04d76934523150be02edc7abb2b2fe349c76c5de
Tiago Amaral <tiagoamral23@gmail.com>
2025-07-21 12:39:13 -0300

    [NEW] Addon: l10n_br_fiscal_field_nfe40_vnf_in_tree

---
+++ l10n_br_fiscal_field_nfe40_vnf_in_tree/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Fiscal Field Nfe40 Vnf In Tree",
+    "summary": """l10n_br_fiscal_field_nfe40_vnf_in_tree""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": [
+        "l10n_br_fiscal",
+    ],
+    "data": [
+        "views/document_view.xml",
+    ],
+    "demo": [],
+}
