d6e40affe46eaa39347674cbbee4776ce2084e53
Tiago Amaral <tiagoamral23@gmail.com>
2025-09-30 07:32:12 -0300

    [NEW] Addon: mail_sale_order_remove_notification_button

---
+++ mail_sale_order_remove_notification_button/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mail Sale Order Remove Notification Button",
+    "summary": """KMEE""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["mail"],
+    "data": [
+        "views/mail_templates.xml",
+    ],
+    "demo": [],
+}
