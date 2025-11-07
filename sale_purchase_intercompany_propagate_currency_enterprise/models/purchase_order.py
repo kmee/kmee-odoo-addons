f86166c1d392e5d1aa8f6d856efa1704c39bb6da
Diego Paradeda <diego.paradeda@kmee.com.br>
2025-11-07 15:07:20 -0300

    [FIX] sale_purchase_intercompany: allow creating multicompany SO with dynamic pricelist (regardless or other company partner pricelist)

--- sale_purchase_intercompany_propagate_currency_enterprise/models/purchase_order.py
+++ sale_purchase_intercompany_propagate_currency_enterprise/models/purchase_order.py
@@ -2,7 +2,8 @@
 # @author Diego Paradeda <diego.paradeda@kmee.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class PurchaseOrder(models.Model):
@@ -10,16 +11,24 @@ class PurchaseOrder(models.Model):
 
     is_intercompany_partner = fields.Boolean(
         compute="_compute_is_intercompany_partner",
+        readonly=False,
         store=True,
     )
     partner_pricelist_id = fields.Many2one(
+        compute="_compute_is_intercompany_partner",
         comodel_name="product.pricelist",
-        string="Pricelist",
-        readonly=False,
+        string="Interco Pricelist",
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        store=True,
     )
     partner_company_id = fields.Many2one(
+        compute="_compute_is_intercompany_partner",
         comodel_name="res.company",
         string="Partner Company",
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        store=True,
     )
 
     @api.depends("partner_id")
@@ -46,4 +55,36 @@ class PurchaseOrder(models.Model):
         if self.partner_pricelist_id:
             res["pricelist_id"] = self.partner_pricelist_id.id
             # res["currency_id"] = self.currency_id.id
+        return res
+
+    def button_approve(self, force=False):
+        for order in self:
+            if order.currency_id != order.partner_pricelist_id.currency_id:
+                raise ValidationError(
+                    _(
+                        "You cannot create SO from PO because sale price list currency "
+                        "is different than purchase price list currency.\n"
+                        "Please fix it manually before approving the PO.\n\n"
+                    )
+                )
+        return super().button_approve(force=force)
+
+    def inter_company_create_sale_order(self, company):
+        """
+        Temporarilly force currency_id in sale order to match partner property pricelist
+        currency.
+
+        Store the original currency and restore it after calling super.
+        """
+
+        original_currencies = {}
+        for rec in self:
+            original_currencies[rec.id] = rec.currency_id
+            rec.currency_id = rec.partner_id.property_product_pricelist.currency_id
+
+        res = super().inter_company_create_sale_order(company)
+
+        for rec in self:
+            rec.currency_id = original_currencies[rec.id]
+
         return res
