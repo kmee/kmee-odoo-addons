6ae0b405050caefaa105df25023f78e6aa00055f
Luis Malta <luis.malta@kmee.com.br>
2026-01-16 13:30:18 -0300

    [MIG] purchase_order_form_matrix_product_link to 18.0

---
+++ purchase_order_form_matrix_product_link/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Purchase Order Form Matrix Product Link",
+    "summary": "Restore product link in purchase order matrix form",
+    "version": "18.0.1.0.0",
+    "category": "Purchase",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "license": "LGPL-3",
+    "depends": ["purchase_product_matrix"],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
