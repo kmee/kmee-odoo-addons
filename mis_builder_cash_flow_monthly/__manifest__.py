a8e471b8311af1d7227902cd5ef1e761af5f5e1a
mileo <mileo@kmee.com.br>
2026-03-15 11:25:03 -0300

    [NEW] mis_builder_cash_flow_monthly: cash flow views for 12 and 24 months
    Creates two new MIS report instances using the existing
    mis_builder_cash_flow template with monthly periods instead of weekly.

---
+++ mis_builder_cash_flow_monthly/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MIS Builder Cash Flow Monthly",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE, Odoo Community Association (OCA)",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["mis_builder_cash_flow"],
+    "data": [
+        "data/mis_report_instance.xml",
+    ],
+    "installable": True,
+}
