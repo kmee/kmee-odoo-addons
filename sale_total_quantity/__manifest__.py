4ba99fc02c08fe3a37fb66bed12f9c71950284db
mileo <mileo@kmee.com.br>
2026-04-01 16:33:28 -0300

    [MIG] sale_total_quantity: Migration to 18.0

---
+++ sale_total_quantity/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Total Quantity",
+    "summary": "Displays the total quantity of products in the sale order.",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "author": "KMEE, Odoo Community Association (OCA)",
+    "maintainers": ["mileo"],
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
