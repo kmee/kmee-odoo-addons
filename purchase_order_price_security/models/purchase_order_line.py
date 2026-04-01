d9d814ca8ef459142bdfbf0dabcb58cf849d8bde
mileo <mileo@kmee.com.br>
2026-04-01 16:31:50 -0300

    [MIG] purchase_order_price_security: Migration to 18.0

---
+++ purchase_order_price_security/models/purchase_order_line.py
@@ -0,0 +1,21 @@
+# Copyright 2024 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PurchaseOrderLine(models.Model):
+
+    _inherit = "purchase.order.line"
+
+    can_edit_price_purchase = fields.Boolean(
+        string="Can Edit Price in Purchase",
+        compute="_compute_can_edit_price_purchase",
+    )
+
+    def _compute_can_edit_price_purchase(self):
+        has_group = self.env.user.has_group(
+            "purchase_order_price_security.group_purchase_order_price_editor"
+        )
+        for line in self:
+            line.can_edit_price_purchase = has_group
