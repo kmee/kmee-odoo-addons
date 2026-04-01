f28f8d0c1806d201f1130c247b59495dd82f1339
mileo <mileo@kmee.com.br>
2026-04-01 16:34:38 -0300

    [MIG] sale_order_line_qty_fields: Migration to 18.0

---
+++ sale_order_line_qty_fields/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Line Qty Fields",
+    "summary": "Display additional quantity fields on sale order lines",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["sale_stock"],
+    "data": [
+        "security/security.xml",
+        "views/res_config_settings.xml",
+        "views/sale_order.xml",
+    ],
+    "demo": [],
+}
