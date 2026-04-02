5d330169e15637291e74d42c40d44a97bf9a5f9c
mileo <mileo@kmee.com.br>
2026-04-01 23:26:47 -0300

    [MIG] sale_commission_period_26: Migration to 16.0

---
+++ sale_commission_period_26/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2026 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Commission Period 26",
+    "version": "16.0.1.0.0",
+    "summary": "Allow the commission to close from the 26th to the 25th of each month.",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["commission"],
+    "installable": True,
+}
