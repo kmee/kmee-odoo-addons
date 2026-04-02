eb8c45fcd77dca8f69652cd36081942df4ce235b
mileo <mileo@kmee.com.br>
2026-04-02 00:02:46 -0300

    [MIG] account_payment_term_manual: Migration to 16.0

--- account_payment_term_manual/models/account_payment_term_manual_mixin.py
+++ account_payment_term_manual/models/account_payment_term_manual_mixin.py
@@ -7,6 +7,7 @@ from odoo import fields, models
 class AccountPaymentTermManualMixin(models.AbstractModel):
 
     _name = "account.payment.term.manual.mixin"
+    _description = "Account Payment Term Manual Mixin"
 
     manual_payment_term_id = fields.Many2one(
         comodel_name="account.payment.term.manual",
