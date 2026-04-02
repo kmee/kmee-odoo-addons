2f9a244e5ece0cde80e61505f2b8f3a4c04149db
mileo <mileo@kmee.com.br>
2026-04-01 23:29:05 -0300

    [MIG] sale_commission_manual_invoice: Migration to 16.0

---
+++ sale_commission_manual_invoice/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Commission Manual Invoice",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE",
+    "website": "https://github.com/KMEE/kmee-odoo-addons",
+    "depends": ["account_commission", "commission"],
+    "data": [
+        "views/sale_commission_settlement.xml",
+    ],
+    "demo": [],
+    "installable": True,
+}
