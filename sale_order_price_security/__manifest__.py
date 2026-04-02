1a1b9b39a118bc466bff18917d8f9ba11db31c95
mileo <mileo@kmee.com.br>
2026-04-01 23:57:15 -0300

    [MIG] sale_order_price_security: Migration to 16.0

--- sale_order_price_security/__manifest__.py
+++ sale_order_price_security/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Price Security",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE",
     "website": "https://github.com/KMEE/kmee-odoo-addons",
