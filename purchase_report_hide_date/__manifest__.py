6ba9756e430aa496e62e08bae4bb2e952cd2facf
mileo <mileo@kmee.com.br>
2026-04-01 23:58:22 -0300

    [IMP] purchase_report_hide_date: fix manifest for 16.0
    - Fix copyright typo (extra bracket)
    - Change license from LGPL-3 to AGPL-3
    - Add maintainers

--- purchase_report_hide_date/__manifest__.py
+++ purchase_report_hide_date/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2024 KMEE]
+# Copyright 2024 KMEE
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Purchase Report Hide Date",
     "version": "16.0.1.0.0",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "author": "KMEE",
     "website": "https://github.com/KMEE/kmee-odoo-addons",
     "depends": [
@@ -13,4 +13,5 @@
     "data": [
         "report/purchase_report.xml",
     ],
+    "maintainers": ["mileo"],
 }
