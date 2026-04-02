855571b79928f05dbbbbcdfca920f0cd94154b92
mileo <mileo@kmee.com.br>
2026-04-02 00:01:12 -0300

    [MIG] sale_total_quantity: Migration to 16.0

--- sale_total_quantity/__manifest__.py
+++ sale_total_quantity/__manifest__.py
@@ -1,15 +1,16 @@
 {
     "name": "Sale Total Quantity",
     "summary": "Displays the total quantity of products in the sale order.",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales",
-    "author": "KMEE",
+    "author": "KMEE, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["sale"],
     "website": "https://github.com/KMEE/kmee-odoo-addons",
     "data": [
         "views/sale_order.xml",
     ],
+    "maintainers": ["mileo"],
     "installable": True,
     "application": False,
 }
--- sale_total_quantity/models/sale_order.py
+++ sale_total_quantity/models/sale_order.py
@@ -1,19 +1,24 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     total_sale_product = fields.Integer(
-        string="Total Products:", compute="_compute_totals"
+        string="Total Products",
+        compute="_compute_totals",
     )
-    total_sale_quantity = fields.Integer(
-        string="Total Quantities:", compute="_compute_totals"
+    total_sale_quantity = fields.Float(
+        string="Total Quantities",
+        compute="_compute_totals",
     )
 
+    @api.depends("order_line.product_id", "order_line.product_uom_qty")
     def _compute_totals(self):
         for record in self:
-            record.total_sale_product = len(set(record.order_line.mapped("product_id")))
+            record.total_sale_product = len(
+                set(record.order_line.mapped("product_id"))
+            )
             record.total_sale_quantity = sum(
                 record.order_line.mapped("product_uom_qty")
             )
