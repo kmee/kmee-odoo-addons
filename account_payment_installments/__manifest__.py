6175e7552fa4f1a37d82582ba5f4c61f0e920972
mileo <mileo@kmee.com.br>
2026-04-01 23:34:20 -0300

    [MIG] account_payment_installments: Migration to 18.0

--- account_payment_installments/__manifest__.py
+++ account_payment_installments/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Account Payment Installments",
     "summary": """Account payment installments""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE,Odoo Community Association (OCA)",
     "website": "https://github.com/KMEE/kmee-odoo-addons",
-    "depends": ["account"],
+    "depends": ["account", "payment"],
     "data": [
         "views/account_payment_term.xml",
     ],
