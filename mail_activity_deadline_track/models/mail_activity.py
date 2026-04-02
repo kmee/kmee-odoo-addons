aa6a49325636c50673ef60b567324bb30bf7e6e2
mileo <mileo@kmee.com.br>
2026-04-02 08:12:46 -0300

    [FIX] mail_activity_deadline_track: use named placeholders in translation

--- mail_activity_deadline_track/models/mail_activity.py
+++ mail_activity_deadline_track/models/mail_activity.py
@@ -13,10 +13,12 @@ class MailActivity(models.Model):
                 if str(activity.date_deadline) != str(vals["date_deadline"]):
                     self.env[activity.res_model].browse(activity.res_id).message_post(
                         body=_(
-                            "Deadline alterado: %s<br/>%s -> %s",
-                            activity.summary or activity.activity_type_id.name or "",
-                            activity.date_deadline,
-                            vals["date_deadline"],
+                            "Deadline alterado: %(summary)s<br/>%(old)s -> %(new)s",
+                            summary=activity.summary
+                            or activity.activity_type_id.name
+                            or "",
+                            old=activity.date_deadline,
+                            new=vals["date_deadline"],
                         )
                     )
         return super().write(vals)
