99f6a471a5ada412bc482d21877d4d32ca426957
mileo <mileo@kmee.com.br>
2026-04-01 23:59:40 -0300

    [MIG] account_custom_payment_term: Migration to 18.0

--- account_custom_payment_term/__manifest__.py
+++ account_custom_payment_term/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Account Custom Payment Term",
     "summary": "Adds record-specific editable payment terms that are archived by default.",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "author": "KMEE",
     "license": "AGPL-3",
