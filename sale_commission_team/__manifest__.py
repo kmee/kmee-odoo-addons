0795abf7a89135d0595066bf07ec57bc0ebfc8cb
mileo <mileo@kmee.com.br>
2026-04-02 06:13:55 -0300

    [MIG] sale_commission_team: Migration to 16.0

--- sale_commission_team/__manifest__.py
+++ sale_commission_team/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Commission Team",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE, Odoo Community Association (OCA)",
     "category": "Sales",
