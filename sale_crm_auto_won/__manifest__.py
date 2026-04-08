da07e6241acf1d91e679889516bc29c477d2e417
mileo <mileo@kmee.com.br>
2026-04-08 13:56:39 -0300

    [MIG] sale_crm_auto_won: Migration to 18.0

--- sale_crm_auto_won/__manifest__.py
+++ sale_crm_auto_won/__manifest__.py
@@ -3,11 +3,11 @@
 
 {
     "name": "Sale Crm Auto Won",
-    "summary": """KMEE""",
-    "version": "16.0.1.0.0",
+    "summary": "Automatically marks CRM opportunity as Won when sale order is confirmed",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "KMEE, Odoo Community Association (OCA)",
     "website": "https://github.com/KMEE/kmee-odoo-addons",
     "depends": ["sale_crm"],
-    'installable': False,
+    "installable": True,
 }
